c44886f41d74ac1c47f02a9761a4c15e4ce3c058
Durga-velu <durgadevi134babu@gmail.com>
2025-03-28 23:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let displayTime=document.getElementById("timer")
+
+let currentDate=new Date()
+
+setInterVal(
+	function () {
+		
+	currentDate=new Date();
+		displayTime.innerHTML=currentDate.toLocaleString();
+	},1000)
